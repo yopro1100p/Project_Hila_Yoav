e595273f8ad15ac6784e3dcb1abd0ae2ccc149d4
hilla2702 <hillali2702@gmail.com>
2024-08-27 14:37:55 +0300

    add 2024-06-13_23414_predictable file

--- Run_Chanels.py
+++ Run_Chanels.py
@@ -1,57 +1,42 @@
 import random
 import time
-from Chanels import ChannelAnalyzer
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from openpyxl import load_workbook
 
-file_path = "C:/Users/user/Desktop/bar ilan/Forth year/project/your_file.h5"  # yoav's link
-# file_path = "C:/Users/dvirg/OneDrive/Desktop/toar1/year4/project/CODE/your_file.h5"  # hilla's link
+from Chanels import ChannelAnalyzer
 
-# Initialize the Excel file
+# File paths
+file_path = "C:/Users/dvirg/OneDrive/Desktop/toar1/year4/project/CODE/2024-02-01T14-33-39McsRecording_MEA21009_predictable_baseline_A-00020.h5"
 output_file = "C:/Users/user/Desktop/bar ilan/Forth year/Project_Hila_Yoav/channel_analysis.xlsx"
-
-
-
-start_time = time.time()  # Record the start time
 
 # Initialize a list to collect all max_values_time for setting xlim
 all_max_values_time = []
 
 # Iterate over electrodes
-for channel_id in range(1, 3):
-    analyzer = ChannelAnalyzer(file_path, channel_id)
-    analyzer.get_channel_data(channel_id)
-    print(analyzer.find_num_of_spikes())
-
-print("Analysis complete. Results saved")
-end_time = time.time()  # Record the end time
-elapsed_time = end_time - start_time  # Calculate the elapsed time
-print(f"Elapsed time: {elapsed_time} seconds")
-
-
-"""
-# Iterate over electrodes
-for electrode in range(1, 120):
-    analyzer = Chanels.ChannelAnalyzer(file_path, electrode)
-    analyzer.find_max_in_groups()
-    analyzer.save_to_excel()
-
+for electrode in range(0, 120):  # Assuming electrodes range from 1 to 120
+    analyzer = ChannelAnalyzer(file_path, electrode)
+    analyzer.get_channel_data(electrode)
+    
     # Print max_values_time for debugging
-    print(f"Electrode {electrode + 1}, max_values_time: {analyzer.max_values_time}", color="red")
+    print(f"Electrode {electrode+1}, max_values_time: {analyzer.max_values_time}")
 
     # Collect max_values_time for setting xlim
     all_max_values_time.extend(analyzer.max_values_time)
 
     # Plot spikes for the current electrode
-    plt.scatter(analyzer.max_values_time, [electrode + 1] * len(analyzer.max_values_time), color='blue', s=1)
+    plt.scatter(analyzer.max_values_time, [electrode+1] * len(analyzer.max_values_time), color='blue', s=1)
 
-
-ChannelAnalyzer.get_channel_data()
 # Check if we have collected any time values for setting xlim
 if all_max_values_time:
     plt.xlim(min(all_max_values_time), max(all_max_values_time))
 else:
     print("No max_values_time found.")
-"""
+
+# Render the plot
+plt.xlabel('Time (s)')
+plt.ylabel('Electrode')
+plt.title('Raster Plot of Electrode Spikes')
+plt.show()
