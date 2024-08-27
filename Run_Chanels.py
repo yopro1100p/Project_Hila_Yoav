1b583e0ea902bcc0636b79688c8a8ecd33e7d251
hilla2702 <hillali2702@gmail.com>
2024-08-27 18:31:55 +0300

    MOVE FILES

--- Run_Chanels.py
+++ Run_Chanels.py
@@ -9,14 +9,18 @@ from openpyxl import load_workbook
 from Chanels import ChannelAnalyzer
 
 # File paths
-file_path = "C:/Users/dvirg/OneDrive/Desktop/toar1/year4/project/CODE/2024-02-01T14-33-39McsRecording_MEA21009_predictable_baseline_A-00020.h5"
-output_file = "C:/Users/user/Desktop/bar ilan/Forth year/Project_Hila_Yoav/channel_analysis.xlsx"
+file_path ="C:/Users/dvirg/OneDrive/Desktop/toar1/year4/project/CODE/recorders/2024-06-13T17-53-27McsRecording_MEA23414_predictable_afterstim_A-00020.h5"
+
+# Extract date and MEA ID from file path
+date_part = file_path.split('/')[-1].split('T')[0]
+mea_id = file_path.split('_')[3][3:8]  # Extract "23414" from "MEA23414"
 
 # Initialize a list to collect all max_values_time for setting xlim
 all_max_values_time = []
 
 # Iterate over electrodes
-for electrode in range(0, 120):  # Assuming electrodes range from 1 to 120
+for electrode in range(0, 3):  # Assuming electrodes range from 1 to 120
+    print(electrode)
     analyzer = ChannelAnalyzer(file_path, electrode)
     analyzer.get_channel_data(electrode)
     
@@ -35,8 +39,15 @@ if all_max_values_time:
 else:
     print("No max_values_time found.")
 
-# Render the plot
+# Set the title using the desired format
+plot_title = f"raster_plot_{date_part}_{mea_id}_"
+plt.title(plot_title)
+
+# Save the plot with the same name as the title
+output_filename = f"{plot_title}.png"
+plt.savefig(output_filename)
+
+# Show the plot
 plt.xlabel('Time (s)')
 plt.ylabel('Electrode')
-plt.title('Raster Plot of Electrode Spikes')
 plt.show()
